edb6d6fe018ab4db3e3c988a471a2a66a67110ab
Karol3Navarro <karo.navarro@duocuc.cl>
2024-09-07 15:42:43 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RegistrAPP_P1_Alvarez_Navarro',
+  webDir: 'www'
+};
+
+export default config;
