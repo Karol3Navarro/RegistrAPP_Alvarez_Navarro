91bf5dd5805a2bc4815cc85f236781a65ac1a5e9
Karol3Navarro <karo.navarro@duocuc.cl>
2024-09-07 19:49:15 -0400

    Redireccion y componentes de Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  
+  public username: string;
+  public password: any;
 
-  constructor() { }
+  constructor(private navCtrl: NavController, private router: Router) { 
+    this.username='';
+    this.password='';
+  }
+
+  login(){
+    if (this.username=="Docente"&& this.password=="12345"){
+      this.navCtrl.navigateForward(['/docente'], {queryParams: {nombre: 'Docente'}})
+    } if (this.username=="Alumno"&& this.password=="12345"){
+      let extras: NavigationExtras = {
+        state: { user: this.username }
+      }
+      this.router.navigate(['/alumno'], extras);
+    }else{
+      alert('Error')
+    }
+  }
 
   ngOnInit() {
   }
