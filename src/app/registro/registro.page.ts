ed32ebf7f2ce6733d6a323d551df0216830f7e38
day-alvarez <135995100+day-alvarez@users.noreply.github.com>
2024-09-08 16:34:02 -0300

    estetico del login y agrego cosas en el registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
   constructor() { }
 
   ngOnInit() {
+   
   }
 
+
+  register() {
+
+    console.log('Username:', this.username);
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    console.log('Confirm Password:', this.confirmPassword);
+
+    if (this.password !== this.confirmPassword) {
+      console.error('Passwords do not match!');
+      return;
+    }
+
+   
+  }
 }
+
