25894b1c954b134e320a7e15013e37dbc6e8027e
day-alvarez <135995100+day-alvarez@users.noreply.github.com>
2024-09-08 18:05:39 -0300

    alumno modificado

--- src/app/alumno/alumno.page.ts
+++ src/app/alumno/alumno.page.ts
@@ -7,8 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./alumno.page.scss'],
 })
 export class AlumnoPage implements OnInit {
-
   username: string = 'guest';
+  submenu: boolean = false; // Agrega esta propiedad
 
   constructor(private router: Router) { 
     const state = this.router.getCurrentNavigation()?.extras?.state;
@@ -19,11 +19,16 @@ export class AlumnoPage implements OnInit {
 
   ngOnInit() {
   }
+
   openMenu() {
     const menu = document.querySelector('ion-menu');
     if (menu) {
       (menu as HTMLIonMenuElement).open();
     }
+  }
+
+  togglesubmenu() { // Asegúrate de tener esta función
+    this.submenu = !this.submenu;
   }
 
   logout() {
