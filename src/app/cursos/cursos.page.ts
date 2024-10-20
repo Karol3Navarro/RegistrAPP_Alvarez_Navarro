3cb9e6ae5ce3aa576a74d833428488fa25afba66
day-alvarez <135995100+day-alvarez@users.noreply.github.com>
2024-10-19 21:49:07 -0300

    cursos

--- src/app/cursos/cursos.page.ts
+++ src/app/cursos/cursos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PresenteprofeService } from '../services/presenteprofe.service';
 
 @Component({
   selector: 'app-cursos',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosPage implements OnInit {
 
-  constructor() { }
+  cursos: any[] = [];  // Aquí se guardarán los cursos obtenidos
+  usuario: string = 'profesor@presenteprofe.cl';  // Aquí puedes definir el usuario que desees
+
+  constructor(private presenteprofeService: PresenteprofeService) {}
 
   ngOnInit() {
+    this.cargarCursos();  // Cargar los cursos al iniciar el componente
   }
 
+  cargarCursos() {
+    this.presenteprofeService.getCursos(this.usuario).subscribe(
+      (response) => {
+        this.cursos = response.cursos;  // Asignamos los cursos obtenidos a la variable 'cursos'
+        console.log(this.cursos);  // Muestra los cursos en la consola
+      },
+      (error) => {
+        console.error('Error al cargar los cursos', error);
+      }
+    );
+  }
+
+  ingresarCurso(cursoId: string) {
+    console.log('Ingresando al curso con ID:', cursoId);
+    // Lógica adicional para ingresar al curso
+  }
 }
--- src/app/services/presenteprofe.service.ts
+++ src/app/services/presenteprofe.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -6,16 +6,23 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PresenteprofeService {
-  private apiURL = 'https://www.presenteprofe.cl/api/v1';
+  private apiURL = 'https://www.presenteprofe.cl/api/v1';  
+
   constructor(private http: HttpClient) { }
 
-
-  login(username: string, password: string):Observable<any>{
-    const url = this.apiURL+"/auth";
-    const payload ={
+  login(username: string, password: string): Observable<any> {
+    const url = `${this.apiURL}/auth`;  
+    const payload = {
       correo: username,
       password: password
-    }
+    };
     return this.http.post(url, payload);
   }
+
+  getCursos(username: string): Observable<any> {
+    const url = `${this.apiURL}/cursos`;  
+    const params = new HttpParams().set('user', username); // Aquí se configuran los parámetros de la URL
+    return this.http.get<any>(url, { params });  // Pasamos los parámetros en el objeto de opciones
+  }
 }
+
